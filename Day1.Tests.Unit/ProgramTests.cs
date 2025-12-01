2e51efac59661f2d2e22130a71586aece8d48ef5
Reinier de Vries <reiniervries@outlook.com>
2025-12-01 18:44:24 +0100

    Add day 1 answer

--- Day1.Tests.Unit/ProgramTests.cs
+++ Day1.Tests.Unit/ProgramTests.cs
@@ -5,5 +5,17 @@ public class ProgramTests
 	[Fact]
 	public void Test()
 	{
+		Assert.Equal(6, Program.DeterminePasswordOnDial([
+			"L68",
+			"L30",
+			"R48",
+			"L5",
+			"R60",
+			"L55",
+			"L1",
+			"L99",
+			"R14",
+			"L82"
+		]));
 	}
 }
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,48 @@ public class Program
 	public static async Task Main(string[] args)
 	{
 		var input = (await File.ReadAllLinesAsync("../../../input"))
-			.Select(values => values.Split(Environment.NewLine));
+			.SelectMany(values => values.Split(Environment.NewLine));
 
+		Console.WriteLine(DeterminePasswordOnDial(input));
+	}
+
+	public static int DeterminePasswordOnDial(IEnumerable<string> input)
+	{
+		var startsAt = 50;
+
+		var inputs = input.Select(current => (Direction: current[0] == 'L' ? -1 : 1, Number: (int.Parse(current[1..]))));
+		var password = 0;
+
+		foreach ((int Direction, int Number) tuple in inputs)
+		{
+			for (int i = 0; i < tuple.Number; i++)
+			{
+				startsAt += tuple.Direction;
+
+				if (startsAt < 0)
+				{
+					startsAt = 99;
+				}
+
+				if (startsAt > 99)
+				{
+					startsAt = 0;
+				}
+
+				// Second half of puzzle:
+				if (startsAt == 0)
+				{
+					password++;
+				}
+			}
+
+			// First half of puzzle:
+			// if (startsAt == 0)
+			// {
+			// 	password++;
+			// }
+		}
+
+		return password;
 	}
 }
