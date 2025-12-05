402af97c83bc234ad520106747680bfd20467b1e
Reinier de Vries <reiniervries@outlook.com>
2025-12-05 20:25:48 +0100

    Add day 5

---
+++ Day5.Tests.Unit/ProgramTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Day5.Tests.Unit;
+
+public class ProgramTests
+{
+	[Fact]
+	public void ExampleInput()
+	{
+		Assert.Equal((3, 14),
+			Program.Solve([
+				"3-5",
+				"10-14",
+				"16-20",
+				"12-18",
+				"12-18",
+				"",
+				"1",
+				"5",
+				"8",
+				"11",
+				"17",
+				"32",
+			]));
+	}
+
+	[Fact]
+	public void MultipleOverlappingRanges()
+	{
+		Assert.Equal((4, 13),
+			Program.Solve([
+				"1-3",
+				"2-4",
+				"3-4",
+				"4-6",
+				"2-8",
+				"10-14",
+				"",
+				"1",
+				"5",
+				"8",
+				"11",
+				"17",
+				"32",
+			]));
+	}
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day5;
+
+public class Program
+{
+	public static async Task Main()
+	{
+		var allLines = (await File.ReadAllLinesAsync("../../../input"));
+
+		Console.WriteLine(Solve(allLines));
+	}
+
+	public static (int CurrentFreshIngredients, long TotalFreshIngredients) Solve(string[] allLines)
+	{
+		var listDelimiter = string.Empty;
+
+		var ranges = allLines
+			.TakeWhile(s => s != listDelimiter)
+			.Select(i => i.Split('-'))
+			.Select((longs => (Start: long.Parse(longs.First()), End: long.Parse(longs.Last()))))
+			.OrderBy(r => r.Start)
+			.ToList();
+
+		var merged = new List<(long Start, long End)>();
+		foreach (var currentRange in ranges)
+		{
+			if (merged.Count == 0 || currentRange.Start > merged[^1].End + 1)
+			{
+				merged.Add(currentRange);
+			}
+			else
+			{
+				var last = merged[^1];
+				merged[^1] = (last.Start, Math.Max(last.End, currentRange.End));
+			}
+		}
+
+		var currentFreshIngredients = allLines
+			.SkipWhile(s => s != listDelimiter)
+			.Skip(1)
+			.Select(long.Parse)
+			.Count(ingredient => ranges.Any(v => ingredient >= v.Start && ingredient <= v.End));
+
+		return (currentFreshIngredients, merged.Sum(r => r.End - r.Start + 1));
+	}
+}
